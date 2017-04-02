1c5e4319a26e408fc75129b2b125e2dca37b62c8
André Lui <andre.lui00@gmail.com>
2017-04-02 14:33:35 -0300

    Arquivo ino
    Arquivo do código utilizado para a leitura do sensor DHT11 e envio dos valores via serial.

---
+++ sketch_mar31a.ino
@@ -0,0 +1,34 @@
+//Programa : Sensor de umidade e temperatura DHT11
+//Autor : Arduino e Cia
+
+#include <dht.h>
+
+#define dht_dpin A1 //Pino DATA do Sensor ligado na porta Analogica A1
+#define min_delay 1
+#define ms_delay min_delay*60*1000
+
+dht DHT; //Inicializa o sensor
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);//Aguarda 1 seg antes de acessar as informações do sensor
+}
+
+void loop()
+{
+  DHT.read11(dht_dpin); //Lê as informações do sensor
+
+  Serial.print(DHT.humidity);
+  Serial.print(";");
+  Serial.print(DHT.temperature);
+  Serial.print(";");
+  Serial.println();
+
+  //Não diminuir o valor abaixo. O ideal é a leitura a cada 15 segundos -> Limitante ThingSpark
+  //delay(15000);  
+
+  unsigned long startMillis = millis();
+  while(millis() - startMillis < (unsigned long)ms_delay);
+  //while(millis() - startMillis < 2000);
+}
